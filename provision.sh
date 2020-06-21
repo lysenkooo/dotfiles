b43ba1e22b96d54f602be2add9015440ccf4ffea
Denis Lysenko <crashcube@gmail.com>
2020-06-21 19:10:01 +0300

    Fix

--- provision.sh
+++ provision.sh
@@ -7,6 +7,8 @@ if [[ ! -x /usr/bin/gcc ]]; then
     xcode-select --install
 fi
 
+echo -e "#!/bin/sh\necho \"Xcode 11.4.1\\nBuild version 11E503a\"\nexit 0" > /usr/local/bin/xcodebuild
+
 sudo xcodebuild -license accept
 
 defaults write -g NSRequiresAquaSystemAppearance 1
@@ -14,6 +16,12 @@ defaults write -g ApplePressAndHoldEnabled 0
 defaults write com.apple.systempreferences TMShowUnsupportedNetworkVolumes 1
 defaults write com.apple.desktopservices DSDontWriteNetworkStores 1
 defaults write com.apple.TextEdit RichText 0
+
+tmutil addexclusion ~/Applications
+tmutil addexclusion ~/Downloads
+tmutil addexclusion ~/Library/Caches/com.docker.docker
+find ~/Projects -type d -name tmp -maxdepth 5 -prune -exec tmutil addexclusion {} \; > /dev/null
+find ~/Projects -type d -name node_modules -maxdepth 5 -prune -exec tmutil addexclusion {} \; > /dev/null
 
 if [[ ! -x /usr/local/bin/brew ]]; then
     echo "Installing Homebrew..."
