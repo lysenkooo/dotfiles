d64390bebd459c011197cccc069124c67fefbe36
Denis Lysenko <crashcube@gmail.com>
2021-12-11 10:51:14 +0300

    WIP

--- provision.sh
+++ provision.sh
@@ -7,16 +7,10 @@ if [[ ! -x /usr/bin/gcc ]]; then
     xcode-select --install
 fi
 
-softwareupdate --install-rosetta
-
-defaults write -g ApplePressAndHoldEnabled 0
-defaults write com.apple.systempreferences TMShowUnsupportedNetworkVolumes 1
-defaults write com.apple.desktopservices DSDontWriteNetworkStores 1
-defaults write com.apple.TextEdit RichText 0
-
-tmutil addexclusion ~/Applications
-tmutil addexclusion ~/Downloads
-tmutil addexclusion ~/Library/Caches/com.docker.docker
+if [[ ! -f /Applications/Rosetta.app ]]; then
+    echo "Installing Rosetta..."
+    softwareupdate --install-rosetta
+fi
 
 if [[ ! -x /opt/homebrew/bin/brew ]]; then
     echo "Installing Homebrew..."
@@ -27,4 +21,29 @@ brew update
 brew doctor
 brew bundle
 
-ansible-playbook playbook.yml
+cd assets && find . -type f -print0 |
+while IFS= read -r -d '' line; do
+    # mkdir -p "~/$line"
+    # ln -s
+    # echo $(dirname $line)
+    ROOT_DIR=`dirname "$line" | cut -c 3-`
+    FILE_PATH=`echo "$line" | cut -c 3-`
+    DST_PATH="~/$FILE_PATH"
+    SRC_PATH="$(pwd)/$FILE_PATH"
+
+    if [ ! -z "$ROOT_DIR" ]; then
+        mkdir -p "$ROOT_DIR"
+    fi
+
+    echo "$DST_PATH << $SRC_PATH"
+    ln -sf "$SRC_PATH" "$DST_PATH"
+done
+
+defaults write -g ApplePressAndHoldEnabled 0
+defaults write com.apple.systempreferences TMShowUnsupportedNetworkVolumes 1
+defaults write com.apple.desktopservices DSDontWriteNetworkStores 1
+defaults write com.apple.TextEdit RichText 0
+
+tmutil addexclusion ~/Applications
+tmutil addexclusion ~/Downloads
+tmutil addexclusion ~/Library/Caches/com.docker.docker
