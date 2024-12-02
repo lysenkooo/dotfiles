a8f0ee3bf56845652685029638928c0d75a1c655
Denis Lysenko <crashcube@gmail.com>
2024-12-02 12:05:00 +0300

    WIP

--- misc/usr/local/sbin/startup.sh
+++ misc/usr/local/sbin/startup.sh
@@ -2,12 +2,10 @@
 
 echo "Startup at $(date)" >> /tmp/startup.log 2>&1
 
-echo "Wait" >> /tmp/startup.log 2>&1
-sleep 10
-
+sleep 5
 echo "Configure TTL" >> /tmp/startup.log 2>&1
 sysctl -w net.inet.ip.ttl=65 >> /tmp/startup.log 2>&1
 
+sleep 5
 echo "Remap tilda" >> /tmp/startup.log 2>&1
-sleep 1
 hidutil property --matching '{"ProductID":0x29a}' --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}' >> /tmp/startup.log 2>&1
